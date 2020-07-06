60d3aa45b96ec38786db79126e1bc4fb107d1d28
Philip Bathe <philip.bathe@investmentsoftwareltd.com>
2020-07-06 11:42:12 +0100

    Try again

---
+++ Assets/Scripts/DealCards.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Linq;
+
+public class DealCards : MonoBehaviour
+{
+    public List<GameObject> Areas;
+    public GameObject Pack;
+    //public GameObject DealerPuck;
+
+    private List<GameObject> cards = new List<GameObject>();
+    private int dealerIndex = 0;
+    public List<GameObject> areaHands;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach(Transform child in Pack.transform)
+        {
+            //Debug.Log("child");
+            if (child.tag == "Card")
+            {
+                cards.Add(child.gameObject);
+            }
+        }
+
+        foreach(var area in Areas)
+        {
+            foreach(Transform child in area.transform)
+            {
+                if (child.tag == "Hand")
+                {
+                    areaHands.Add(child.gameObject);
+                }
+            } 
+        }
+    }
+
+    // Update is called once per frame
+    public void OnClick()
+    {
+        ChangeDealer();
+        CollectCards();
+        Deal();
+    }
+
+    void ChangeDealer()
+    {
+        dealerIndex++;
+        if(dealerIndex >= Areas.Count)
+        {
+            dealerIndex = 0;
+        }
+    }
+
+    void CollectCards()
+    {
+        foreach(var area in areaHands)
+        {
+            foreach(Transform child in area.transform)
+            {
+                if (child.tag == "Card")
+                {
+                    Destroy(child.gameObject);
+                }
+            }
+        }
+    }
+
+    void Deal()
+    {
+        List<GameObject> shuffledCards = cards.OrderBy(a => Guid.NewGuid()).ToList();
+
+        for(var i = 0; i < shuffledCards.Count; i++)
+        {
+            GameObject playerCard = Instantiate(shuffledCards[i], new Vector3(0, 0, 0), Quaternion.identity);
+
+            var areaIndex = (i + dealerIndex) % Areas.Count;
+
+            playerCard.transform.SetParent(areaHands[areaIndex].transform, false);
+        }
+    }
+}
