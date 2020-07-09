3c08127def173644d30c5ba9345fd5d34b042e6a
Philip Bathe <philip.bathe@investmentsoftwareltd.com>
2020-07-09 11:41:13 +0100

    Create Pack at start of scene

---
+++ Assets/Scripts/CreatePack.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CreatePack : MonoBehaviour
+{
+    public GameObject CardPrefab;
+
+    private string[] suits = new string[] {"Club", "Diamond", "Heart", "Spade"};
+
+    void Start()
+    {
+        Debug.Log("Creating Pack");
+
+        string cardName = string.Empty;
+        string id = string.Empty;
+
+        foreach(var suit in suits)
+        {
+            for(int i = 1; i < 14; i++)
+            {
+                id = $"0{i}";
+                cardName = suit + id.Substring(id.Length - 2);
+                addCard(cardName);
+            }
+        }
+    }
+
+    private void addCard(string cardName)
+    {
+        GameObject newCard = Instantiate(CardPrefab, this.transform.position, this.transform.rotation);
+        newCard.name = cardName;
+        newCard.GetComponent<Image>().sprite = Resources.Load<Sprite>($"Sprites/{cardName}");
+
+        newCard.transform.SetParent(this.transform, false);
+    }
+
+
+}
