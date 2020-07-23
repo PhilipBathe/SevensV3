15a555b55debacc97c7c4cb97c5219018d244810
Philip Bathe <philip.bathe@investmentsoftwareltd.com>
2020-07-23 20:51:58 +0100

    Steal some graphics and start on login scene

---
+++ Assets/Scripts/LoginManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LoginManager : MonoBehaviour
+{
+   public InputField playerNameField;
+
+    void Start()
+    {
+        playerNameField.text = PlayerPrefs.GetString("username").Trim();
+    }
+
+    public void JoinTable() 
+    {
+        PlayerPrefs.SetString("username", playerNameField.text);
+        PlayerPrefs.Save();
+        //Debug.Log(name.text);
+
+        SceneManager.LoadScene("TableScene");
+    }
+}
